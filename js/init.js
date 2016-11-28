b4f50316a78cd0f853d423693f04a9e248f7a18c
Lee <lee@Lees-MacBook-Pro.local>
2016-11-28 15:09:18 -0600

    changed more stuff

--- js/init.js
+++ js/init.js
@@ -4,5 +4,21 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    var options = [
+    {selector: '#box-1', offset: 20, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#box-2', offset: 60, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#box-3', offset: 100, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#box-4', offset: 140, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+  ];
+  Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
