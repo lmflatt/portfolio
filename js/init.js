3ec86658b0309a86525efac2781de6f75aa1a236
lmflatt <lee.m.flatt@gmail.com>
2016-11-30 21:31:44 -0600

    added work cards, need images

--- js/init.js
+++ js/init.js
@@ -17,6 +17,33 @@
     {selector: '#box-4', offset: 140, callback: function(el) {
       Materialize.fadeInImage($(el));
     } },
+    {selector: '#stagger-list-1', offset: 150, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } },
+    {selector: '#stagger-list-2', offset: 150, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } },
+    {selector: '#stagger-list-3', offset: 150, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } },
+    {selector: '#card-1', offset: 60, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#card-2', offset: 100, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#card-3', offset: 140, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#card-4', offset: 60, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#card-5', offset: 100, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '#card-6', offset: 140, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
   ];
   Materialize.scrollFire(options);
 
